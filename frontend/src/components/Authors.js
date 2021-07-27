3fd989d6a427a437436979cd07a8abe0c9193783
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2021-07-27 16:05:23 +0200

    Forgot to remove .git file from cloned frontend, now adding submodule code into main

---
+++ frontend/src/components/Authors.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Authors = () => {
+	const authors = []
+
+	return (
+		<div>
+			<h2>authors</h2>
+			<table>
+				<tbody>
+					<tr>
+						<th></th>
+						<th>born</th>
+						<th>books</th>
+					</tr>
+					{authors.map((a) => (
+						<tr key={a.name}>
+							<td>{a.name}</td>
+							<td>{a.born}</td>
+							<td>{a.bookCount}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	)
+}
+
+export default Authors
