3a837e825bdaea2c61b6c319a6cea03fb8cca6e1
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2021-08-18 17:25:48 +0200

    Finished exercise 8.19, filtering books by genre using a quite complex solution, not sure if this exercise was meant to be this challenging

--- frontend/src/components/Books.js
+++ frontend/src/components/Books.js
@@ -1,9 +1,10 @@
 import { useQuery } from '@apollo/client'
-import React from 'react'
+import React, { useState } from 'react'
 import { GET_BOOKS } from '../queries'
 
 const Books = () => {
 	const { data } = useQuery(GET_BOOKS)
+	const [filter, setFilter] = useState(null)
 
 	return (
 		<div>
@@ -17,15 +18,52 @@ const Books = () => {
 						<th>published</th>
 					</tr>
 					{data &&
-						data.allBooks.map((book) => (
-							<tr key={book.title}>
-								<td>{book.title}</td>
-								<td>{book.author}</td>
-								<td>{book.published}</td>
-							</tr>
-						))}
+						data.allBooks
+							.filter((book) => {
+								return filter ? book.genres.includes(filter) : book
+							})
+							.map((book) => {
+								return (
+									<tr key={book.title}>
+										<td>{book.title}</td>
+										<td>{book.author}</td>
+										<td>{book.published}</td>
+									</tr>
+								)
+							})}
 				</tbody>
 			</table>
+
+			<div>
+				{data &&
+					data.allBooks
+						.reduce((initialValue, currentGenre) => {
+							return [...initialValue, ...currentGenre.genres]
+						}, [])
+						.sort()
+						.filter((genres, i, arr) => {
+							return genres !== arr[i - 1]
+						})
+						.map((genre) => {
+							return (
+								<button
+									onClick={() => {
+										setFilter(genre)
+									}}
+									key={genre}
+								>
+									{genre}
+								</button>
+							)
+						})}
+				<button
+					onClick={() => {
+						setFilter(null)
+					}}
+				>
+					All Books
+				</button>
+			</div>
 		</div>
 	)
 }
