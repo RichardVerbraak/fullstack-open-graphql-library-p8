2845e53823fe45aaf9397daf181cb3c827feffc0
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2021-09-01 19:22:18 +0200

    Added websocket link to the apollo client for subscriptions

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,7 +9,19 @@ import {
 	InMemoryCache,
 	ApolloProvider,
 	HttpLink,
+	split,
 } from '@apollo/client'
+
+// Setup for subscriptions (requires websockets)
+import { WebSocketLink } from '@apollo/client/link/ws'
+import { getMainDefinition } from '@apollo/client/utilities'
+
+const wsLink = new WebSocketLink({
+	uri: 'ws://localhost:4000/subscriptions',
+	options: {
+		reconnect: true,
+	},
+})
 
 const authLink = setContext((_, { headers }) => {
 	// Get token from storage
@@ -26,9 +38,21 @@ const authLink = setContext((_, { headers }) => {
 
 const httpLink = new HttpLink({ uri: 'http://localhost:4000' })
 
+const splitLink = split(
+	({ query }) => {
+		const definition = getMainDefinition(query)
+		return (
+			definition.kind === 'OperationDefinition' &&
+			definition.operation === 'subscription'
+		)
+	},
+	wsLink,
+	httpLink
+)
+
 const client = new ApolloClient({
 	cache: new InMemoryCache(),
-	link: authLink.concat(httpLink),
+	link: authLink.concat(splitLink),
 })
 
 ReactDOM.render(
