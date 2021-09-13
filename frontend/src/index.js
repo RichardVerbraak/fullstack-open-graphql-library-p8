92cdb004a0a36c05f853b012b5817190827cbe16
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2021-09-13 15:53:37 +0200

    Connection setup to the websocket on the client completed but the subscription itself is no longer working all of the sudden

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -16,12 +16,12 @@ import {
 import { WebSocketLink } from '@apollo/client/link/ws'
 import { getMainDefinition } from '@apollo/client/utilities'
 
-// const wsLink = new WebSocketLink({
-// 	uri: 'ws://localhost:4000/',
-// 	options: {
-// 		reconnect: true,
-// 	},
-// })
+const wsLink = new WebSocketLink({
+	uri: 'ws://localhost:4000/graphql',
+	options: {
+		reconnect: true,
+	},
+})
 
 const authLink = setContext((_, { headers }) => {
 	// Get token from storage
@@ -38,21 +38,21 @@ const authLink = setContext((_, { headers }) => {
 
 const httpLink = new HttpLink({ uri: 'http://localhost:4000' })
 
-// const splitLink = split(
-// 	({ query }) => {
-// 		const definition = getMainDefinition(query)
-// 		return (
-// 			definition.kind === 'OperationDefinition' &&
-// 			definition.operation === 'subscription'
-// 		)
-// 	},
-// 	wsLink,
-// 	httpLink
-// )
+const splitLink = split(
+	({ query }) => {
+		const definition = getMainDefinition(query)
+		return (
+			definition.kind === 'OperationDefinition' &&
+			definition.operation === 'subscription'
+		)
+	},
+	wsLink,
+	httpLink
+)
 
 const client = new ApolloClient({
+	link: authLink.concat(splitLink),
 	cache: new InMemoryCache(),
-	link: authLink.concat(httpLink),
 })
 
 ReactDOM.render(
