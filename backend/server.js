f27a135a5c5a885e9895dc007080010f705908eb
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2021-09-13 16:01:26 +0200

    Fixed issue of the server not connecting to the subscription server (served it at localhost:4000 but shouldve been localhost:4000/graphql)

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const { ApolloServer } = require('apollo-server-express')
 
 // Setting up subscription support
 const express = require('express')
-const { createServer } = require('http')
+const http = require('http')
 
 const { execute, subscribe } = require('graphql')
 const { SubscriptionServer } = require('subscriptions-transport-ws')
@@ -62,7 +62,7 @@ if (process.argv[2] === 'delete') {
 const startApolloServer = async (typeDefs, resolvers) => {
 	// Required logic for integrating with Express
 	const app = express()
-	const httpServer = createServer(app)
+	const httpServer = http.createServer(app)
 
 	// Setting up Subscription (doesn't take typeDefs and Resolvers but it does take an executable GraphQL schema)
 	const schema = makeExecutableSchema({ typeDefs, resolvers })
@@ -82,9 +82,7 @@ const startApolloServer = async (typeDefs, resolvers) => {
 		],
 		context: async ({ req }) => {
 			const auth = req ? req.headers.authorization : null
-
 			const bearer = auth && auth.split(' ')[0].toLowerCase()
-
 			// Checks for token
 			if (auth && bearer) {
 				// Verify token
@@ -92,10 +90,8 @@ const startApolloServer = async (typeDefs, resolvers) => {
 					auth.split(' ')[1],
 					process.env.JWT_SECRET
 				)
-
 				// Find user and populate the friends field
 				const loggedInUser = await User.findById(decodedToken.id)
-
 				return { loggedInUser }
 			}
 		},
@@ -119,16 +115,16 @@ const startApolloServer = async (typeDefs, resolvers) => {
 
 	// More required logic for integrating with Express
 	await server.start()
+
 	server.applyMiddleware({
 		app,
-
 		// By default, apollo-server hosts its GraphQL endpoint at the
 		// server root. However, *other* Apollo Server packages host it at
 		// /graphql. Optionally provide this to match apollo-server.
-		path: '/',
+		// path: '/',
 	})
 	// Modified server startup
-	await httpServer.listen({ port: 4000 })
+	httpServer.listen({ port: 4000 })
 	console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
 }
 
